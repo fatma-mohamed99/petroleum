0ba6d7a0d862bd34ddde866be2c8a251ae42c196
fatma-mohamed99 <fatma.99.mph@gmail.com>
2025-05-07 17:29:43 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid w-full bg-gradient-to-l from-main to-transparent ">
+      <h1 className="text-header-lg text-main font-bold">Heading Large</h1>
+      <h2 className="text-header-sm text-secondary font-semibold">Heading Small</h2>
+      <p className="text-title-lg">Title lg</p>
+      <p className="text-desc-sm">Small description here.</p>
+
+    </div>
+  );
+}
