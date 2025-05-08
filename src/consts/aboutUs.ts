a741cbdc46140b111d2b6e961629915c57692f39
Mostapha Yasser <mostaphyasser18@gmail.com>
2025-05-09 00:37:21 +0300

    edit-footer

---
+++ src/consts/aboutUs.ts
@@ -0,0 +1,22 @@
+export const aboutUsLinks = [
+    {
+      title: "Overview",
+      target: "/"
+    },
+      {
+        title: "Mission Statement",
+        target: "/"
+      },
+      {
+        title: "Vision Statement",
+        target: "/"
+      },
+      {
+        title: "Quality & Safety Statement",
+        target: "/"
+      },
+      {
+        title: "Organization Chart",
+        target: "/"
+      }
+    ];
