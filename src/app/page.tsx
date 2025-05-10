54e2800cb74ba72f9ba62fa96d6becb834d6c9ad
Mostapha Yasser <mostaphyasser18@gmail.com>
2025-05-10 16:21:03 +0300

    chande bg for section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ProjectSection from "@/components/home-component/project-section/ProjectS
 import CertificatesSection from "@/components/home-component/certificate-section/CertificatesSection";
 export default function Home() {
   return (
-    <div className="grid w-full   ">
+    <div className="grid w-full mb-30   ">
       <div className="inset-0 ">
         <HeroCarousel />
       </div>
@@ -15,28 +15,30 @@ export default function Home() {
         <OurLegacy />
       </div>
       
-      <hr className="w-full sm:w-11/12 md:w-10/12  mx-auto h-1 text-white  bg-main/10 mb-10 " />
 
       {/* ###################################### */}
+      <div className="w-full bg-main/5">
 
       <div className="w-full  sm:w-11/12 md:w-10/12   mx-auto py-12 my-7">
           <CertificatesSection />
       </div>
+
+      </div>
       {/* ###################################### */}
 
-      <hr className="w-full sm:w-11/12 md:w-10/12  mx-auto h-1 text-white  bg-main/10  " />
 
       <div className="py-12 my-5   ">
         <ProjectSection />
       </div>
 
       {/* ###################################### */}
-      <hr className="w-full sm:w-11/12 md:w-10/12  mx-auto h-1 text-white  bg-main/10  mt-8" />
+      <div className="w-full bg-main/5">
 
       <div className="w-full sm:w-11/12 md:w-10/12  mx-auto py-12  ">
           <Specialties />
       </div>
       {/* ###################################### */}
+    </div>
     </div>
   );
 }
