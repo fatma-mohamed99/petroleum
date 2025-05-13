06a53d92f146978053528ead0871475354b80ee6
Mostapha Yasser <mostaphayasser18@gmail.com>
2025-05-13 17:35:47 +0300

    Gsap

---
+++ src/components/home-component/Who-are-we-section/WhoAreWeSection.tsx
@@ -0,0 +1,62 @@
+// "use client"
+// import { useRef } from 'react';
+// import { useGSAP } from '@gsap/react';
+// import gsap from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// // import { Lenis as ReactLenis } from '@studio-freight/react-lenis';
+// import slideImage1 from "../../../../public/images/hero-img/slider1.webp";
+// import slideImage2 from "../../../../public/images/hero-img/slider2.jpg";
+// import slideImage3 from "../../../../public/images/hero-img/slider3.jpg";
+// import slideImage4 from "../../../../public/images/hero-img/slider4.jpg";
+// import slideImage5 from "../../../../public/images/hero-img/slider5.jpg";
+// import slideImage6 from "../../../../public/images/hero-img/slider6.jpg";
+// // import Image from 'next/image';
+
+// function WhoAreWeSection() {
+//   const containerRef = useRef<HTMLDivElement>(null);
+// //   const images = [slideImage1, slideImage2, slideImage3, slideImage4, slideImage5, slideImage6];
+  
+//   useGSAP(() => {})
+  
+
+
+//   return (
+//     // <ReactLenis root>
+//       <div ref={containerRef} className="main who-we-are-section py-20">
+//         <div className="main-content mx-auto px-4">
+//           <div className="text-center mb-16">
+//             <p className="text-header-lg font-medium">Who Are We</p>
+//           </div>          
+//           <div className="max-w-3xl mx-auto content-text copy">
+//             <div className='line'>
+//                 <p>
+// Over the years,Unico petroleum developed a
+//                reputation across the region
+//               as a reliable provider of services in the Oil and Gas industry.
+//                 </p>
+//             </div>
+//              <div className='line'>
+//                 <p>
+                  
+//               Over the years,Unico petroleum developed a
+//                reputation across the region
+//               as a reliable provider of services in the Oil and Gas industry.  
+//                 </p>
+//             </div>
+//              <div className='line'>
+//             <p className="">
+//               Its commitment to the highest standards of quality, safety, and service level has been
+//                evident
+//               in the variety of projects it has completed on time and within budget and continues to be
+//               at the heart of the company vision for the future.
+//             </p>
+             
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     // </ReactLenis>
+//   );
+// }
+
+// export default WhoAreWeSection;
