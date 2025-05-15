278f60147d7a790ac4e287817ef2b142891f0f19
fatma-mohamed99 <fatma.99.mph@gmail.com>
2025-05-15 15:38:48 +0300

    edit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,11 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "react-hooks/exhaustive-deps": "off",
-      "@next/next/no-img-element": "off",
-    },
+
   },
 ];
 
